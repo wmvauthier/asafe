d27c81233e02963a6854f61d66b23a0d30eddc9e
C1321100 Wesley Matheus Vauthier <c1321100@interno.bb.com.br>
2025-01-14 00:08:54 -0300

    Commit Inicial

--- scripts.js
+++ scripts.js
@@ -243,8 +243,67 @@ function carregarMusicas() {
     .catch((err) => console.error("Erro ao carregar as músicas:", err));
 }
 
+// Função para carregar e preencher as músicas
+function carregarRepertorio() {
+  fetch("musicas.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const content = document.querySelector(".repertorio");
+      // Guardar as músicas carregadas
+      const musicas = data;
+
+      // Iterar pelas músicas na escala
+      musicas.forEach((musica) => {
+        // Encontrar a música completa pelo ID
+
+        const col = document.createElement("div");
+        col.classList.add("col-lg-3", "col-sm-6", "col-6", "mb-4");
+        col.style["padding-left"] = "5px";
+        col.style["padding-right"] = "5px";
+        col.style["margin-bottom"] = "0px!important";
+
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const link = document.createElement("a");
+        link.href = "https://www.youtube.com/watch?v=" + musica.referLink;
+        link.target = "_blank";
+
+        const img = document.createElement("img");
+        img.src = "https://img.youtube.com/vi/" + musica.referLink + "/0.jpg";
+        img.alt = `Thumbnail de ${musica.titulo}`;
+
+        const h3 = document.createElement("h3");
+        h3.textContent = musica.titulo;
+        h3.style["font-size"] = "1rem";
+        h3.style["padding-top"] = "10px";
+        h3.style["padding-bottom"] = "1px";
+        h3.style["font-weight"] = "bold";
+        h3.style["color"] = "white";
+
+        const h32 = document.createElement("h3");
+        h32.textContent = musica.artista;
+        h32.style["font-size"] = "1rem";
+        h32.style["padding-top"] = "0px";
+        h32.style["padding-bottom"] = "1px";
+        h32.style["color"] = "grey";
+
+        link.appendChild(img);
+        card.appendChild(link);
+        card.appendChild(h3);
+        card.appendChild(h32);
+        col.appendChild(card);
+
+        content.appendChild(col);
+        
+      });
+    })
+    .catch((err) => console.error("Erro ao carregar as músicas:", err));
+}
+
 // Chamadas para carregar os dados ao carregar a página
 window.onload = function () {
   carregarIntegrantes();
   carregarMusicas();
+  carregarRepertorio();
 };
